b2fd27f4820d3139ed9a046b9cc8af01cc4f51e1
Alce Kerrigan <imor231@gmail.com>
2020-02-24 10:44:23 -0500

    Fitness

--- Code/FitnessFunction.java
+++ Code/FitnessFunction.java
@@ -36,18 +36,37 @@ class FitnessFunction{
 //  COMPUTE A CHROMOSOME'S RAW FITNESS *************************************
 
 	public long dis(Point a, Point b) {
-		return Math.sqrt((a.x * a) + (b * b));
+		return Math.sqrt(Math.pow((a.x - b.x), 2) + Math.pow((a.x - b.x), w);
 	}
 
-	public void doRawFitness(Chromo X){
-		X.rawFitness = 0;
-		for (int i = 0; i < Parameters.geneSize - 1; i++) {
-			X.rawFitness += ()
+	public TSPChromo ordmap(TSPChromo X) {
+		TSPChromo retval = new TSPChromo();
+		HashSet<Integer> used = new HashSet<Integer>();
+
+		for (int i = 0; i < Parameters.geneSize; i++) {
+			int j = X.chromo.get(i);
+			for (; used.contains(j % Parameters.geneSize); j++);
+			retval.chromo.add(i, j);
+		}
+		return retval;
+	}
+
+	public void doRawFitness(TSPChromo X){
+		if (Parameters.ordFlag) {
+			TSPChromo temp = ordmap(X);
+			for (int i = 0; i < Parameters.geneSize - 1; i++) {
+				X.rawFitness += dis(Search.cities.get(temp.chromo.get(i)), Search.cities.get(temp.chromo.get(i)));
+			}
+		}
+		else {
+			for (int i = 0; i < Parameters.geneSize - 1; i++) {
+				X.rawFitness += dis(Search.cities.get(X.chromo.get(i)), Search.cities.get(X.chromo.get(i)));
+			}
 		}
 
 //  PRINT OUT AN INDIVIDUAL GENE TO THE SUMMARY FILE *********************************
 
-	public void doPrintGenes(Chromo X, FileWriter output) throws java.io.IOException{
+	public void doPrintGenes(TSPChromo X, FileWriter output) throws java.io.IOException{
 		System.out.println("Executing FF Gene Output");
 	}
 
