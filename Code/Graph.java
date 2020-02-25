3308534ab547f740c587341939ca29f756929e43
Ian Valle <ianvalle27@gmail.com>
2020-02-25 15:31:17 -0500

    Create Graph.java

---
+++ Code/Graph.java
@@ -0,0 +1,12 @@
+public class Graph
+{
+	public long best;
+	public long average;
+	public long stdDev;
+	
+	public Graph (long best, long average, long stdDev)
+	{
+		this.best = best;
+		this.average = average;
+		this. stdDev = stdDev;
+	}
