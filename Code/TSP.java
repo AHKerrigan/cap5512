eb4c45145a813d25603952bbb14f88b09b65dd63
Alce Kerrigan <imor231@gmail.com>
2020-02-25 12:01:02 -0500

    Small changes

--- Code/TSP.java
+++ Code/TSP.java
@@ -36,30 +36,37 @@ public long dis(Point a, Point b) {
 	return retval;
 }
 
-public TSPChromo ordmap(TSPChromo X) {
-	TSPChromo retval = new TSPChromo();
+public ArrayList<Integer> ordmap(TSPChromo X) {
+	ArrayList<Integer> retval = new ArrayList<>();
 	HashSet<Integer> used = new HashSet<Integer>();
 
 	for (int i = 0; i < Parameters.geneSize; i++) {
 		int j = X.chromo.get(i);
 		for (; used.contains(j % Parameters.geneSize); j++);
-		retval.chromo.add(i, j);
+		retval.add(j % Parameters.geneSize);
+		used.add(j);
 	}
 	return retval;
 }
 
 public void doRawFitness(TSPChromo X){
 	if (Parameters.ordflag) {
-		TSPChromo temp = ordmap(X);
-		for (int i = 0; i < Parameters.geneSize - 2; i++) {
-			X.rawFitness += dis(Search.cities.get(temp.chromo.get(i)), Search.cities.get(temp.chromo.get(i + 1)));
+		ArrayList<Integer> temp = ordmap(X);
+		for (int i : temp) {
+			System.out.print(i + " ");
 		}
+		System.out.println();
+		for (int i = 0; i < Parameters.geneSize - 1; i++) {
+			X.rawFitness += dis(Search.cities.get(temp.get(i)), Search.cities.get(temp.get(i + 1)));
+		}
+		X.rawFitness += dis(Search.cities.get(temp.get(Parameters.geneSize - 1)), Search.cities.get(temp.get(0)));
 	}
 	else {
-		//System.out.println();
 		for (int i = 0; i < Parameters.geneSize - 1; i++) {
 			X.rawFitness += dis(Search.cities.get(X.chromo.get(i)), Search.cities.get(X.chromo.get(i + 1)));
 		}
+		X.rawFitness += dis(Search.cities.get(X.chromo.get(Parameters.geneSize - 1)), Search.cities.get(X.chromo.get(0)));
+
 	}
 	//System.out.println("fitness is "  + X.rawFitness);
 }
